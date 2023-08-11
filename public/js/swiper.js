ce2c5c4283a0909dfab9c2bbc94faef9ff95d223
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-11 16:43:36 -0500

    required Bulma, swiper nav btn

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -5,6 +5,10 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
 
