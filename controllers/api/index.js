009e81a56f8f6238143b249dc20b1a3b6699f2c3
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-14 15:11:52 -0500

    user GET routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const bookingsRoutes = require('./bookings.js');
+const userRoutes = require('./user');
+// const bookingsRoutes = require('./bookings.js');
 // const projectRoutes = require('./projectRoutes');
 
-// router.use('/users', userRoutes);
-router.use('/bookings', bookingsRoutes);
+router.use('/users', userRoutes);
+// router.use('/bookings', bookingsRoutes);
 
 module.exports = router;
