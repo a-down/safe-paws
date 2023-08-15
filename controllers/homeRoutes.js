608295219df4b23474954eb58bb54e0aea9f69a3
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-15 18:05:49 -0500

    first part of booking form populates on load

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const randomPets = require('../utils/randomPets')
 const withAuth = require('../utils/auth')
-const { User, Pets } = require('../models')
+const { User, Pets, Services } = require('../models')
 
 
 
@@ -17,7 +17,13 @@ router.use('/login', async (req, res) => {
 })
 
 router.use('/booking', async (req, res) => {
-  res.render('booking')
+  const userData = await User.findByPk(1, {include: [{model: Pets}]})
+  const user = userData.get({ plain: true });
+
+  const serviceData = await Services.findAll().catch((err) => res.status(500).json(err))
+  const services = serviceData.map((service) => service.get({plain: true}))
+  
+  res.render('booking', { user, services })
 })
 
 router.use('/staff', async (req, res) => {
@@ -26,7 +32,7 @@ router.use('/staff', async (req, res) => {
 
 router.use('/*', async (req, res) => {
   const petData = await Pets.findAll().catch((err) => res.status(500).json(err))
-  const pets = petData.map((data) => data.get({plain: true}))
+  const pets = petData.map((pet) => pet.get({plain: true}))
   console.log(pets)
 
   let petsArr = randomPets(pets)
