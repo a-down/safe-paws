7c5a19c1b6cd2055271f693a206966df56f5fe69
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-15 12:11:37 -0500

    handlebars loads 3 pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
 
+
+
+router.use('/profile', async (req, res) => {
+  res.render('profile')
+})
+
+router.use('/login', async (req, res) => {
+  res.render('login');
+})
+
 router.use('/', async (req, res) => {
   res.render('homepage');
 })
