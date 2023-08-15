af8ac38061039aed47a925a141b2341cb1ade827
Lauren Bleyl <laurenbleyl@gmail.com>
2023-08-15 12:34:04 -0500

    rogue branch

---
+++ controllers/api/userLogin.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../../models/User');
+
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res.status(404).json({ message: 'Login failed. Please try again!' });
+      return;
+    }
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      userData.password
+    );
+    if (!validPassword) {
+      res.status(400).json({ message: 'Login failed. Please try again!' });
+      return;
+    }
+
+    req.session.save(()=> {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+  
+      res.json({ user: userData, message: 'You are now logged in!'})
+    })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
